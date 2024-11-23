8918ee6e33b10d4a3ddf5dce78927547d38a6b8f
shrutik <shrutikdawane@gmail.com>
2024-11-23 12:17:41 +0530

    added 90% default zoom

--- frontend/src/Pages/Testimonials.tsx
+++ frontend/src/Pages/Testimonials.tsx
@@ -4,19 +4,19 @@ import { ChevronLeft, ChevronRight, Quote } from 'lucide-react';
 const testimonials = [
   {
     id: 1,
-    content: "The journey from being Miss to CA krishna Taparia was a long but fulfilling one. I have so many people to thank for me being what I am today. Foremost my family and friends who have been very patient and supportive throughout the process. ICAI for providing the opportunity so that I can fulfill my dreams. Dina Bama Patil Library is one institute that I cannot thank enough without it. I could have never found that space and zone where I can  spend my dedicated hours. All I needed was patience, hard work, and lots of perseverance's don't give up ever and you will find yourself right next to the finish line with a title in front of your name.",
+    content: "The journey from being Miss to CA krishna Taparia was a long but fulfilling one. I have so many people to thank for me being what I am today.\n Foremost my family and friends who have been very patient and supportive throughout the process. ICAI for providing the opportunity so that I can fulfill my dreams. Dina Bama Patil Library is one institute that I cannot thank enough without it. I could have never found that space and zone where I can  spend my dedicated hours. All I needed was patience, hard work, and lots of perseverance's don't give up ever and you will find yourself right next to the finish line with a title in front of your name.",
     name: "krishna Taparia",
     profession: "chartered accountant"
   },
   {
     id: 2,
-    content: "Finally , a long awaited prefix is here CA Sanket Nailesh Patel. I thank to my parents, ICAI ,Dina Bama Patil library, my mentors who gave me the right direction at right point of time for achieving this success. For achieving this name tag is you should be patience, confident, disciplined and hardworking without this the journey of becoming is incomplete. So there are my views which are required to become a Chartered accountant.",
+    content: "Finally , a long awaited prefix is here CA Sanket Nailesh Patel. I thank to my parents, ICAI ,Dina Bama Patil library, my mentors who gave me the right direction at right point of time for achieving this success. \n For achieving this name tag is you should be patience, confident, disciplined and hardworking without this the journey of becoming is incomplete. So there are my views which are required to become a Chartered accountant.",
     name: "Sanket Nailesh Patel",
     profession: "Chartered Accountant"
   },
   {
     id: 3,
-    content: "I feel so blessed every time I See my CA final Marksheet, I must say the feeling can't be described in words. Finally I have added prefix CA to my name Sunil Pandey. The dream of becoming a Chartered Accountant is came Through. The discipline, hard work, patience finally paid off I would like to thanks to My Parents, Dina Bama Library ICAI Teachers, friends. I am Chartered Accountant today is just because of you all.",
+    content: "I feel so blessed every time I See my CA final Marksheet, I must say the feeling can't be described in words. Finally I have added prefix CA to my name Sunil Pandey. \n The dream of becoming a Chartered Accountant is came Through. The discipline, hard work, patience finally paid off I would like to thanks to My Parents, Dina Bama Library ICAI Teachers, friends. I am Chartered Accountant today is just because of you all.",
     name: "Sunil Pandey",
     profession: "Chartered Accountant"
   }
